2863fa4d6fb52f38fcd2f50fd74768aa2c0909e4
thiagokido <thiago.ekido@gmail.com>
2021-11-15 13:10:24 -0300

    Post para cadastro de novos usuarios

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -2,6 +2,15 @@ function signupView (req, res) {
     res.render('signup')
 }
 
+function signupCreate (req, res) {
+
+    console.log(req.body)
+
+    res.redirect('/login')
+
+}
+
 module.exports = {
-    signupView
+    signupView,
+    signupCreate
 }
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signupView } = require('../controllers/signupController')
+const { signupView, signupCreate } = require('../controllers/signupController')
 
 /* GET SIGN UP PAGE */
 
 router.get('/', signupView)
+
+router.post('/cadastrar', signupCreate)
 
 module.exports = router;
